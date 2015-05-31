f2cf1fca167f02211acfd0650744b026821956e1
Raveline <eraveline@gmail.com>
2015-05-31 10:45:23 +0200

    bootstrap adapted for new pos system

--- main.py
+++ main.py
@@ -33,36 +33,42 @@ def main():
 
 class Game(object):
     def test_bootstrap(self):
-        def build_area(x, y, x2, y2):
-            area = Selection(x, y)
+        def build_area(x, y, z=None, x2=None, y2=None):
+            if z is None:
+                z = 0
+            if x2 is None:
+                x2 = x
+            if y2 is None:
+                y2 = y
+            area = Selection(x, y, z)
             area.x2 = x2
             area.y2 = y2
             return area
         # Build a storage area
         commands = []
-        commands.append(BuildCommand(build_area(2, 2, 5, 5)))
+        commands.append(BuildCommand(build_area(2, 2, 0, 5, 5)))
         # build a corridor to a tavern room
-        commands.append(BuildCommand(build_area(5, 3, 9, 4)))
-        commands.append(PutCommand(build_area(8, 3, 8, 4), door))
+        commands.append(BuildCommand(build_area(5, 3, 0, 9, 4)))
+        commands.append(PutCommand(build_area(8, 3, 0, 8, 4), door))
         # Build the tavern room
-        commands.append(BuildCommand(build_area(9, 2, 12, 12)))
-        commands.append(PutCommand(build_area(11, 1, 11, 1), door))
+        commands.append(BuildCommand(build_area(9, 2, 0, 12, 12)))
+        commands.append(PutCommand(build_area(11, 1), door))
         # Build the kitchen
-        commands.append(BuildCommand(build_area(1, 7, 7, 14)))
-        commands.append(BuildCommand(build_area(8, 12, 8, 12)))
-        commands.append(PutCommand(build_area(8, 12, 8, 12), door))
+        commands.append(BuildCommand(build_area(1, 7, 0, 7, 14)))
+        commands.append(BuildCommand(build_area(8, 12)))
+        commands.append(PutCommand(build_area(8, 12), door))
         for command in commands:
             bus.bus.publish({'command': command}, bus.WORLD_EVENT)
         commands = []
-        commands.append(RoomCommand(self.tavern.tavern_map.fill_from(3, 3), 1))
+        commands.append(RoomCommand(self.tavern.tavern_map.fill_from((3, 3, 0)), 1))
         commands.append(RoomCommand(
-            self.tavern.tavern_map.fill_from(10, 10), 0))
+            self.tavern.tavern_map.fill_from((10, 10, 0)), 0))
         commands.append(RoomCommand(
-            self.tavern.tavern_map.fill_from(3, 10), 2))
-        commands.append(PutCommand(build_area(9, 11, 9, 11), counter))
-        commands.append(PutCommand(build_area(9, 6, 9, 6), chair))
-        commands.append(PutCommand(build_area(5, 7, 7, 9), oven))
-        commands.append(PutCommand(build_area(4, 7, 4, 7), work_station))
+            self.tavern.tavern_map.fill_from((3, 10, 0)), 2))
+        commands.append(PutCommand(build_area(9, 11), counter))
+        commands.append(PutCommand(build_area(9, 6), chair))
+        commands.append(PutCommand(build_area(5, 7, 0, 7, 9), oven))
+        commands.append(PutCommand(build_area(4, 7), work_station))
         for command in commands:
             bus.bus.publish({'command': command}, bus.WORLD_EVENT)
         self.customers.tick_counter += 100
@@ -102,7 +108,7 @@ class Game(object):
         self.continue_game = True
 
     def display_background(self):
-        display(self.clip_world(self.tavern.tavern_map.tiles,
+        display(self.clip_world(self.tavern.tavern_map.tiles[0],
                                 self.receiver.scape.frame),
                 self.world_console.console)
 
@@ -220,7 +226,8 @@ class Game(object):
 
     def describe_area(self):
         x, y = self.state.navigator.getX(), self.state.navigator.getY()
-        tile = self.tavern.tavern_map.tiles[y][x]
+        pos = (x, y, self.state.navigator.getZ())
+        tile = self.tavern.tavern_map[pos]
         text = "(%d, %d) - %s" % (x, y, tile.describe())
         display_text(self.text_console.console, text, 0, 0)
 
