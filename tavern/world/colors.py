830b593ba80c82cf1fcd0d76fd8d365458aa72a4
Adrien Duclos <adrien.duclos@milibris.com>
2015-12-28 17:28:31 +0100

    Add a list of custom colors for Tavern

---
+++ tavern/world/colors.py
@@ -0,0 +1,8 @@
+import libtcodpy as tcod
+
+ALE_AMBER = tcod.Color(240, 195, 0)
+WINE_RED = tcod.Color(172, 30, 68)
+SPIRITS_ABSINTH = tcod.Color(127, 221, 76)
+MEAT_BLOOD = tcod.Color(133, 6, 6)
+VEGETABLES_APPLE = tcod.Color(52, 201, 36)
+MEAL_BUFF = tcod.Color(208, 192, 122)
