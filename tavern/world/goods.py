2a17e017f035b1ebbae0abd4c2f96f0c5e84ef16
Raveline <eraveline@gmail.com>
2016-01-01 15:19:11 +0100

    Goods should have a blocks property

--- tavern/world/goods.py
+++ tavern/world/goods.py
@@ -54,7 +54,7 @@ class Goods(object):
         self.store_cell_cost = goods_type_to_store_cell_cost[self.goods_type]
         self.character = character
         self.color = color
-        self.block = False
+        self.blocks = False
         self.quality = quality
 
     def __str__(self):
